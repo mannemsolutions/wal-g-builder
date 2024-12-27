10138a3fee5e4809101e83a098cc4978067f7568
Sebastiaan Mannem <sebas@mannem.nl>
2024-12-27 16:29:04 +0100

    Hmmm

--- scripts/build-with-compose.sh
+++ scripts/build-with-compose.sh
@@ -5,6 +5,5 @@ export GITREV
 export BASEIMAGE=${BASEIMAGE:-rockylinux:9}
 export WALGVERSION=${WALGVERSION:-v0.0.0-devel}
 export WALGURL=${WALGURL:-https://github.com/mannemsolutions/wal-g/}
-export DOCKERPLATFORM=${DOCKERPLATFORM:-linux/arm64}
 docker-compose up builder
 find rpms
